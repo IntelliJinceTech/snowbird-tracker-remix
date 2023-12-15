7d16edbe453bf103d75a479888ed77a510f5606d
Christopher Jin <chrisjin1@gmail.com>
2023-12-14 22:51:59 -0500

    fixing eslint errors and parsing for tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div>Hello World</div>;
+  return <div className="text-red-500">Hello World</div>;
 }
 
 export default App;
